fedbb87da77c481f47d6d125dd219bb60fac69bd
ohabardi <127239155+ohabardi@users.noreply.github.com>
2025-05-21 11:17:43 +0300

    Update employee.py

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -1,41 +1,41 @@
-# Import the QueryBase class
-from .query_base import QueryBase
+from employee_events.query_base import QueryBase
+from employee_events.sql_execution import QueryMixin  # technically already inherited, but fine to import
 
-# Import dependencies needed for sql execution
-# from the `sql_execution` module
-from .sql_execution import execute_sql, execute_sql_df
-
-# Define a subclass of QueryBase called Employee
 class Employee(QueryBase):
-
-    # Set the class attribute `name` to the string "employee"
     name = "employee"
 
-    # Define a method called `names` that returns (full_name, id) for all employees
     def names(self):
         query = """
-            SELECT full_name, employee_id
+            SELECT first_name || ' ' || last_name AS full_name, employee_id
             FROM employee
         """
-        return execute_sql(query)
+        return self.query(query)
 
-    # Define a method called `username` that returns the full name for a given employee id
     def username(self, id):
         query = f"""
-            SELECT full_name
+            SELECT first_name || ' ' || last_name AS full_name
             FROM employee
             WHERE employee_id = {id}
         """
-        return execute_sql(query)
+        return self.query(query)
 
-    # Define a method called model_data that returns a pandas DataFrame
     def model_data(self, id):
         query = f"""
-            SELECT SUM(positive_events) positive_events
-                 , SUM(negative_events) negative_events
+            SELECT SUM(positive_events) AS positive_events,
+                   SUM(negative_events) AS negative_events
             FROM {self.name}
             JOIN employee_events
                 USING({self.name}_id)
             WHERE {self.name}.{self.name}_id = {id}
         """
-        return execute_sql_df(query)
+        return self.pandas_query(query)
+def event_counts(self, id):
+    query = f"""
+        SELECT event_date,
+               SUM(positive_events) AS Positive,
+               SUM(negative_events) AS Negative
+        FROM employee_events
+        WHERE employee_id = {id}
+        GROUP BY event_date
+    """
+    return self.pandas_query(query)
