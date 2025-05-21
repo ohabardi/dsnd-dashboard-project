58976cc0cb6081abce97a57e7191cb7a9352f37a
ohabardi <127239155+ohabardi@users.noreply.github.com>
2025-05-21 11:25:59 +0300

    Update utils.py

--- report/utils.py
+++ report/utils.py
@@ -1,14 +1,10 @@
 import pickle
 from pathlib import Path
 
-# Using the Path object, create a `project_root` variable
-# set to the absolute path for the root of this project directory
+# Absolute path to project root
 project_root = Path(__file__).resolve().parents[1]
 
-# Using the `project_root` variable
-# create a `model_path` variable
-# that points to the file `model.pkl`
-# inside the assets directory
+# Path to model file
 model_path = project_root / "assets" / "model.pkl"
 
 def load_model():
