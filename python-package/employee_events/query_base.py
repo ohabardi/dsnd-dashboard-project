7d3da9f5387ff8fcc51fe1e5f22f6f2e7780c7a4
ohabardi <127239155+ohabardi@users.noreply.github.com>
2025-05-21 11:19:04 +0300

    Update query_base.py

--- python-package/employee_events/query_base.py
+++ python-package/employee_events/query_base.py
@@ -1,45 +1,30 @@
-import sqlite3
+from employee_events.sql_execution import QueryMixin
 import pandas as pd
-from pathlib import Path
 
-DB_PATH = Path(__file__).resolve().parent / 'employee_events.db'
-
-class QueryBase:
-    def __init__(self):
-        self.name = ""
+class QueryBase(QueryMixin):
+    # class attribute to define table name in subclasses
+    name = ""
 
     def names(self):
-        """Return list of table names in the database"""
-        with sqlite3.connect(DB_PATH) as conn:
-            query = "SELECT name FROM sqlite_master WHERE type='table';"
-            df = pd.read_sql(query, conn)
-        return df
+        # return all names from table (employee or team)
+        return []
 
     def event_counts(self, id):
-        """Returns count of positive and negative events for a given ID"""
-        self.name = 'employee_events'
         query = f"""
-            SELECT event_date,
-                   SUM(positive_events) as total_positive,
-                   SUM(negative_events) as total_negative
-            FROM {self.name}
-            WHERE employee_id = ?
+            SELECT event_date, 
+                   SUM(positive_events) AS positive_events,
+                   SUM(negative_events) AS negative_events
+            FROM employee_events
+            WHERE {self.name}_id = {id}
             GROUP BY event_date
             ORDER BY event_date;
         """
-        with sqlite3.connect(DB_PATH) as conn:
-            df = pd.read_sql(query, conn, params=(id,))
-        return df
+        return self.pandas_query(query)
 
     def notes(self, id):
-        """Returns notes and note_date for a given ID"""
-        self.name = 'notes'
         query = f"""
             SELECT note_date, note
-            FROM {self.name}
-            WHERE employee_id = ?
-            ORDER BY note_date;
+            FROM notes
+            WHERE {self.name}_id = {id};
         """
-        with sqlite3.connect(DB_PATH) as conn:
-            df = pd.read_sql(query, conn, params=(id,))
-        return df
+        return self.pandas_query(query)
