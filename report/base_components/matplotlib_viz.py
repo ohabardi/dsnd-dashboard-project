baa05ff476e0f345b2abb763ce627cc65beed190
ohabardi <127239155+ohabardi@users.noreply.github.com>
2025-05-21 11:24:34 +0300

    Update matplotlib_viz.py

--- report/base_components/matplotlib_viz.py
+++ report/base_components/matplotlib_viz.py
@@ -1,65 +1,29 @@
-from .base_component import BaseComponent
-
-import matplotlib.pyplot
-from fasthtml.common import Img
-import matplotlib.pylab as plt
-import matplotlib
+from report.base_components.base_component import BaseComponent
 import io
 import base64
-
-# This is necessary to prevent matplotlib from causing memory leaks
-# https://stackoverflow.com/questions/31156578/matplotlib-doesnt-release-memory-after-savefig-and-close
-matplotlib.use('Agg')
-matplotlib.rcParams['savefig.transparent'] = True
-matplotlib.rcParams['savefig.format'] = 'png'
-
-
-def matplotlib2fasthtml(func):
-    '''
-    Copy of https://github.com/koaning/fh-matplotlib, which is currently hardcoding the 
-    image format as jpg. png or svg is needed here.
-    '''
-    def wrapper(*args, **kwargs):
-        # Reset the figure to prevent accumulation. Maybe we need a setting for this?
-        fig = plt.figure()
-
-        # Run function as normal
-        func(*args, **kwargs)
-
-        # Store it as base64 and put it into an image.
-        my_stringIObytes = io.BytesIO()
-        plt.savefig(my_stringIObytes)
-        my_stringIObytes.seek(0)
-        my_base64_jpgData = base64.b64encode(my_stringIObytes.read()).decode()
-
-        # Close the figure to prevent memory leaks
-        plt.close(fig)
-        plt.close('all')
-        return Img(src=f'data:image/jpg;base64, {my_base64_jpgData}')
-    return wrapper
-
+from matplotlib import pyplot as plt
 
 class MatplotlibViz(BaseComponent):
+    def __call__(self, entity_id, model):
+        fig = self.visualization("Visualization", model, entity_id)
+        return self.render_plot(fig)
 
-    @matplotlib2fasthtml
-    def build_component(self, entity_id, model):
-        return self.visualization(entity_id, model)
-    
-    
-    def visualization(self, entity_id, model):
-        pass
+    def visualization(self, name, model, asset_id):
+        raise NotImplementedError("Subclasses must implement visualization()")
 
-    def set_axis_styling(self, ax, bordercolor='white', fontcolor='white'):
-        
-        ax.title.set_color(fontcolor)
-        ax.xaxis.label.set_color(fontcolor)
-        ax.yaxis.label.set_color(fontcolor)
+    def render_plot(self, fig):
+        buf = io.BytesIO()
+        fig.savefig(buf, format='png', bbox_inches='tight')
+        plt.close(fig)
+        buf.seek(0)
+        img_base64 = base64.b64encode(buf.read()).decode('utf-8')
+        return f"<img src='data:image/png;base64,{img_base64}'/>"
 
-        ax.tick_params(color=bordercolor, labelcolor=fontcolor)
+    def set_axis_styling(self, ax, border_color="black", font_color="black"):
         for spine in ax.spines.values():
-            spine.set_edgecolor(bordercolor)
-
-        for line in ax.get_lines():
-            line.set_linewidth(4)
-            line.set_linestyle('dashdot')
-
+            spine.set_color(border_color)
+        ax.xaxis.label.set_color(font_color)
+        ax.yaxis.label.set_color(font_color)
+        ax.title.set_color(font_color)
+        ax.tick_params(axis='x', colors=font_color)
+        ax.tick_params(axis='y', colors=font_color)
